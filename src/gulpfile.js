20207e871e51700350fa118e10f2e8e8c4595e5a
Joel <joel.r.mz@gmail.com>
2015-09-14 16:12:35 -0300

    incluindo framework pixiJS e configurando node,bower e gulp

---
+++ src/gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var bower  = require('gulp-bower');
+
+var vendor_dest = "scripts/vendor";
+
+var vendors_srcs =["bower_components/pixi.js/bin/*"];
+
+
+gulp.task("default",["bower","vendor-files"],function  () {
+	
+});
+
+gulp.task("bower",function  () {
+	return bower();	
+});
+
+gulp.task("vendor-files",function  () {
+	gulp.src(vendors_srcs)
+		.pipe(gulp.dest(vendor_dest));
+});
---
+++ src/readme.txt
@@ -0,0 +1,5 @@
+1º passo instalar node 
+2º passo se npm não estiver configurado nas variaveis do sistema configure.
+PATH do npm: C:\Program Files\nodejs
+3º passo execute o comando "npm install" no prompt na pasta "src" do repositorio
+4º passo execute o comando gulp 
